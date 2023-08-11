c786a3b460ec384d2bf41ca67a0738be09d7e3c3
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-08-11 18:47:19 +0100

    feat!: Remove browser compatibility

--- mod.ts
+++ mod.ts
@@ -1,53 +1,54 @@
-import type { DenoConfigurationFile } from './config-interface.ts'
-import type { ImportMap } from './importmap-interface.ts'
+import type { DenoConfiguration } from './deno-configuration.ts';
+import type { ImportMap } from './import-map.ts';
 
-const isDeno = navigator.userAgent === `Deno/${ Deno?.version?.deno }`
-const isBrowser = !!globalThis.document
+const denoConfigurationFilenames = [
+	'deno.json',
+	'deno.jsonc',
+] as const;
 
-/** Tries to find the Deno configuration file from a list of commonly used names. On failure it returns `undefined`. */
-export async function tryToFindConfig() {
-    const posibleFiles = [
-        'deno.json',
-        'deno.jsonc',
-        'tsconfig.json',
-        'tsconfig.jsonc',
-        'jsconfig.json',
-        'jsconfig.jsonc',
-    ]
+/**
+ * Gets the Deno configuration
+ */
+export async function getDenoConfiguration() {
+	for (const filename of denoConfigurationFilenames) {
+		try {
+			return JSON.parse(
+				await Deno.readTextFile(filename),
+			) as DenoConfiguration;
+		} catch (error) {
+			if (error instanceof Deno.errors.NotFound) continue;
 
-    for (const posibleFile of posibleFiles) {
-        try {
-            return {
-                filepath: posibleFile,
-                content: JSON.parse(await Deno.readTextFile(posibleFile)) as DenoConfigurationFile,
-            }
-        } catch (error) {
-            if (error instanceof Deno.errors.NotFound) continue
-            throw error
-        }
-    }
+			throw error;
+		}
+	}
+
+	return null;
 }
 
-/** Tries to find the import map. In Deno using the `importMap` field from `tryToFindConfig` and in the browser using `document.querySelector('script[type="importmap"]')`. On failure it returns `undefined`. */
-export async function tryToFindImportMap() {
-    if (isDeno) {
-        const importMapPath = (await tryToFindConfig())?.content?.importMap
+/**
+ * Gets the import map.
+ *
+ * ---
+ *
+ * To parse the import map you can use https://github.com/timreichen/importmap (JavaScript) or https://github.com/denoland/import_map (WebAssembly Rust).
+ */
+export async function getImportMap(): Promise<ImportMap | null> {
+	const { imports, scopes, importMap } = await getDenoConfiguration() ?? {};
 
-        return !importMapPath ? undefined : {
-            filename: importMapPath,
-            content: JSON.parse(await Deno.readTextFile(importMapPath)) as ImportMap,
-        }
-    } else if (isBrowser) {
-        try {
-            return {
-                filename: undefined,
-                content: JSON.parse(
-                    (document.querySelector('script[type="importmap"]') as HTMLScriptElement)
-                    ?.innerText
-                ) as ImportMap,
-            }
-        } catch {}
-    }
+	if (imports || scopes) {
+		return {
+			...(imports ? { imports } : {}),
+			...(scopes ? { scopes } : {}),
+		};
+	}
+
+	if (importMap) {
+		return JSON.parse(
+			await Deno.readTextFile(importMap),
+		) as ImportMap;
+	}
+
+	return null;
 }
 
-export type { DenoConfigurationFile, ImportMap }
+export type { DenoConfiguration, ImportMap };
